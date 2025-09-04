dd0e917257f926cdcabbee27ffbff0e74d0e0c3f
lyzzet Valenzuela Cabello <lyzzet102806@gmail.com>
2025-09-04 13:51:42 -0600

    Main modificado

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from clases.avanzadas import Avanzadas
 
+def main():
+    op = Operaciones()
+    op.leerNumeros()
+    op.sumar()
+    op.mostrarResultado()
+    
+if __name__ == "__main__":
+    main()
+    
+    
