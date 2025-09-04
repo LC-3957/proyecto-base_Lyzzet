9ecde457204c3180d659f368ba15cd9829678ac3
lyzzet Valenzuela Cabello <lyzzet102806@gmail.com>
2025-09-04 13:47:12 -0600

    Version 1 Anfitrion lista

---
+++ avanzadas.py
@@ -0,0 +1,25 @@
+class Avanzadas:
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+        self.resultado = 0
+        
+    def leerNumeros(self):
+        while True:
+            try:
+                self.num1 = int(input("Número 1:"))
+                break
+            except Exception:
+                print("Número inválido")
+                continue
+        while True:
+            try:
+                self.num2 = int(input("Número 2:"))
+                break
+            except Exception:
+                print("Número inválido")
+                continue    
+    
+    def elevarPotencia(self):
+        self.resultado= "El numero "+str(self.num1)+" elevado al "+str(self)+" es = "+str(self.num1**self.num2)
+        
